078c7cabafd3df48cc91374bcee427b432673248
Isadora Goedert <isadoragoedert05@gmail.com>
2025-07-08 10:22:17 -0300

    adicionando calculo de triângulo

--- POO/Programa.java
+++ POO/Programa.java
@@ -7,14 +7,27 @@ public class Programa {
         System.out.println("égua");
 
         Triangulo triangulo1 = new Triangulo();
-        triangulo1.ladoA = 10;
-        triangulo1.ladoB = 60;
-        triangulo.lado = 20;
+        triangulo1.ladoA = 3;
+        triangulo1.ladoB = 4;
+        triangulo1.ladoC = 5;
 
 
         Triangulo triangulo2 = new Triangulo();
-        triangulo2.ladoA = 80;
-        triangulo2.ladoB = 50;
-        triangulo2.ladoC = 50;
+        triangulo2.ladoA = 7.5;
+        triangulo2.ladoB = 4.5;
+        triangulo2.ladoC = 4.02;
+        
+        //Triangulo1
+        double p = (triangulo1.ladoA+triangulo1.ladoB+triangulo1.ladoC) / 2;
+        double areaTriangulo1 = Math.sqrt(p*(p - triangulo1.ladoA)*(p - triangulo1.ladoB)*(p - triangulo1.ladoC));
+
+        
+        //Triangulo1
+        p = (triangulo2.ladoA+triangulo2.ladoB+triangulo2.ladoC) / 2;
+        double areaTriangulo2 = Math.sqrt(p*(p - triangulo2.ladoA)*(p - triangulo2.ladoB)*(p - triangulo2.ladoC));
+
+        System.out.println("Area triangulo 1: " +areaTriangulo1);
+        System.out.println("Area triangulo 2: " +areaTriangulo2);
+
     } 
 }
