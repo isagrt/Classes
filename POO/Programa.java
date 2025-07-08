18bcb99437c25530fe02d62627fb4b0bfa93359c
Isadora Goedert <isadoragoedert05@gmail.com>
2025-07-08 10:06:14 -0300

    Estudando classes

---
+++ POO/Programa.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+
+public class Programa {
+    public static void main (String[] args){
+
+        /*Locale.setDefault(new Locale(language: "pt", country: "BR"));*/
+        System.out.println("égua");
+
+        Triangulo triangulo1 = new Triangulo();
+        triangulo1.ladoA = 10;
+        triangulo1.ladoB = 60;
+        triangulo.lado = 20;
+
+
+        Triangulo triangulo2 = new Triangulo();
+        triangulo2.ladoA = 80;
+        triangulo2.ladoB = 50;
+        triangulo2.ladoC = 50;
+    } 
+}
---
+++ POO/Triangulo.java
@@ -0,0 +1,5 @@
+public class Triangulo {
+    double ladoA;
+    double ladoB;
+    double ladoC;
+}
