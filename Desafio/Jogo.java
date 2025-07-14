58065366e08fadd04d8440c7b4ddf8b004e58a16
isagrt <isadoragoedert05@gmail.com>
2025-07-14 10:28:49 -0300

    tentando organizar metodos

--- Desafio/Jogo.java
+++ Desafio/Jogo.java
@@ -4,82 +4,88 @@ public class Jogo{
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         Personagem personagem = new Personagem();
-        Personagem.Narcisa narcisa = new Personagem.Narcisa();
-        Personagem.Rita rita = new Personagem.Rita();
 
 
-        System.out.println("Configuração da personagem Narcisa:");
+        System.out.println("Configuracao da personagem Narcisa:");
         narcisa.definirStatusInicial();
         narcisa.mostrarStatus();
-
+        //narcisa.Historia();
         narcisa.Separador();
 
-        System.out.println("Configuração da personagem Rita:");
+        System.out.println("Configuracao da personagem Rita:");
         rita.definirStatusInicial();
         rita.mostrarStatus();
 
         narcisa.Separador();
 
-        System.out.println("Após uma guerra nuclear que dizimou a humanidade, a Terra ficou em silêncio. Com o passar dos séculos, os animais, únicos sobreviventes, começaram a evoluir! Influenciados por mutações ou antigas forças esquecidas, aprenderam a falar, andar sobre duas patas e formar sociedades. Inspirando-se nos vestígios da civilização humana, construíram um novo mundo baseado na era medieval. Agora, lobos nobres, gatos cavaleiros, corujas sábias e outros animais governam reinos, travam guerras, praticam magia e escrevem sua própria história, talvez repetindo os erros dos antigos senhores da Terra.");
+        System.out.println("Apos uma guerra nuclear que dizimou a humanidade, a Terra ficou em silencio. Com o passar dos seculos, os animais, unicos sobreviventes, comecaram a evoluir!\nInfluenciados por mutacoes ou antigas forcas esquecidas, aprenderam a falar, andar sobre duas patas e formar sociedades.\nInspirando-se nos vestigios da civilizacao humana, construiram um novo mundo baseado na era medieval.\nAgora, lobos nobres, gatos cavaleiros, corujas sabias e outros animais governam reinos, travam guerras, praticam magia e escrevem sua propria historia, talvez repetindo os erros dos antigos senhores da Terra.");
+        System.out.println("");
+        
 
-
-
-
-         while (narcisa.vida > 0 && rita.vida > 0) {
-            System.out.println("Turno de " + narcisa.nome + ":");
-            System.out.println("1 - Atacar");
-            System.out.println("2 - Usar Feitiço");
+         /*while (narcisa.vida > 0 && rita.vida > 0) {
+            System.out.println("Vez " + narcisa.nome + ":");
+            System.out.println("1 - Bater com cajado");
+            System.out.println("2 - Usar cajado encantado");
             System.out.println("3 - Ver Status");
-            System.out.print("Escolha sua ação: ");
+            System.out.print("Escolha sua acao: ");
             int escolhaCisa = scanner.nextInt();
 
         switch (escolhaCisa) {
             case 1:
                 narcisa.AtaqueMago();
                 rita.receberDanoGuerreira();
+                narcisa.mostrarStatus();
                 break;
             case 2:
                 narcisa.usarFeitico();
                 rita.receberDanoGuerreiraEspecial();
+                narcisa.mostrarStatus();
                 break;
             case 3:
                 narcisa.mostrarStatus();
                 continue;
             default:
-                System.out.println("Opção inválida. Turno perdido.");
+                System.out.println("Opçao invalida. Vez de Rita.");
                 break;
                 }
+  
 
     if (rita.vida <= 0) break;
 
     narcisa.Separador();
 
         System.out.println("Vez de " + rita.nome + ":");
-        System.out.println("1 - Atacar");
-        System.out.println("2 - Usar Habilidade");
+        System.out.println("1 - Arranhar com garras");
+        System.out.println("2 - Usar Guitarra magica");
         System.out.println("3 - Ver Status");
-        System.out.print("Escolha sua ação: ");
+        System.out.print("Escolha sua acao: ");
         int escolhaR = scanner.nextInt();
 
         switch (escolhaR) {
             case 1:
                 rita.AtaqueGuerreira();
                 narcisa.receberDanoMago();
+                rita.mostrarStatus();
                 break;
             case 2:
                 rita.usarGuitarra();
                 narcisa.receberDanoMagoEspecial();
+                rita.mostrarStatus();
                 break;
             case 3:
                 rita.mostrarStatus();
                 continue;
              default:
-                System.out.println("Opção inválida. Turno perdido.");
+                System.out.println("Opcao invalida. Vez de Narcisa");
                 break;
         }
 
         narcisa.Separador();
-    }
-
-
+    }*/
+        if(rita.vida <= 0){
+            System.out.println("Parabens Narcisa, voce eh a maior feiticeira deste reino!!");
+        }else{
+            System.out.println("Parabens Rita, voce eh a maior guerreira deste reino!!");
+        }
+    
         }}
--- Desafio/Personagem.java
+++ Desafio/Personagem.java
@@ -1,42 +1,42 @@
 import java.util.Scanner;
 
 public class Personagem{
-    public static class Narcisa{
-        public String nome = "Narcisa";
-        public String oponente = "Rita";
+        public String nome;
+        public String oponente;
+        public String classe;
         public int nivel = 1;
         public int vida = 100;
         public int mana;
         public int forca;
 
-
         public void Separador(){
             System.out.println("------------------------------");
         }
         public void Historia(){
-            System.out.println(" Em um mundo após uma guerra nuclear, só restaram animais na terra, e como consequencia, eles aprenderam a falar, andar e agir igual humanos na idade média. ");
+            String desenho =                                                             
+                                                                                                      
         }
 
         public void definirStatusInicial() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Defina o nível de força para " + nome + ": ");
+        System.out.print("Defina o nivel de forca para " + nome + ": ");
         forca = scanner.nextInt();
 
-        System.out.print("Defina o nível de mana para " + nome + ": LEMBRETE!!: " +nome + " é um mago : ");
+        System.out.print("Defina o nivel de mana para " + nome + "\nLEMBRETE!! " +nome + " eh um" +classe+ "  : ");
         mana = scanner.nextInt();
 
-        System.out.println("Status definidos! Força: " + forca + " | Mana: " + mana);
+        System.out.println("Status definidos! Forca: " + forca + " | Mana: " + mana);
         }
 
 
-        public void AtaqueMago(){
+        public void Ataque(){
             forca += 2;
             mana += 2;
-            System.out.println(nome+" utiliza seu cajado mágico para atacar "+oponente);
-            System.out.println("+1 de força!! Força atual : "+forca);
+            System.out.println(nome+" utiliza seu cajado magico para atacar "+oponente);
+            System.out.println("+1 de forca!! Forca atual : "+forca);
         }
-        public void receberDanoMago(){
+        public void receberDano(){
             
             if(vida > 0){
                 vida -= 2;
@@ -44,32 +44,32 @@ public class Personagem{
                 System.out.println("+5 de dano :( Vida atual : "+vida);
             }
             else{
-                System.out.println("Fim de jogo para"+nome+" :( Parabebéns "+oponente);
+                System.out.println("Fim de jogo para"+nome+" :( Parabens "+oponente);
             }
 
 
         }
-        public void receberDanoMagoEspecial(){
+        public void receberDanoEspecial(){
             if(vida > 0 ){
                 vida -= 10;
                 System.out.println(nome+" tenta desviar do golpe de guitarra de "+oponente+" e infelizmente não consegue :(  ");
                 System.out.println("+10 de dano :( Vida atual : "+vida);
             }
             else{
-                System.out.println("Fim de jogo para"+nome+" :( Parabebéns "+oponente);
+                System.out.println("Fim de jogo para"+nome+" :( Parabens "+oponente);
             }
 
 
         }
-        public void usarFeitico(){
+        public void usarHabilidade(){
             forca += 4; 
             mana -= 4;
-            System.out.println(nome+" lança sua mágia negra para atacar"+oponente);
-            System.out.println("+2 de força!! Força atual: "+forca);
+            System.out.println(nome+" lanca sua magia negra para atacar"+oponente);
+            System.out.println("+2 de forca!! Forca atual: "+forca);
         }
         public void subirDeNivel(){
             if(mana == 75 && forca == 50){
-                System.out.println("   Parabéns você subiu de nível!!");
+                System.out.println(nome+", parabens você subiu de nivel!!");
                 mana += 10;
                 forca += 2;
                 vida += 10;
@@ -78,100 +78,7 @@ public class Personagem{
         }
         public void mostrarStatus(){
             System.out.println
-            ("Nome: "+nome+" | Classe: Mago | Vida: "+vida+" | Força: "+forca+" | Mana: "+mana);
+            ("Nome: "+nome+" | Classe: Mago | Vida: "+vida+" | Forca: "+forca+" | Mana: "+mana);
         }
 
-
-
-
-
-    }
-
-    
-
-    public static class Rita{
-    public String nome = "Rita";
-    public String oponente = "Narcisa";
-    public int nivel = 1;
-    public int vida = 100;
-    public int mana;
-    public int forca;
-    
-
-        public void Separador(){
-            System.out.println("------------------------------");
-        }
-        public void Historia(){
-            System.out.println(" oii");
-        }
-
-        public void definirStatusInicial() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Defina o nível de força para " + nome + " \nLEMBRETE!!:" +nome+ "é um gurreiro ");
-        forca = scanner.nextInt();
-
-        System.out.print("Defina o nível de mana para " + nome + ": ");
-        mana = scanner.nextInt();
-
-        System.out.println("Status definidos! Força: " + forca + " | Mana: " + mana);
-        }
-
-        public void AtaqueGuerreira(){
-            forca += 2;
-            mana += 2;
-            System.out.println(nome+" utiliza seu cajado mágico para atacar "+oponente);
-            System.out.println("+1 de força!! Força atual : "+forca);
-        }
-        public void receberDanoGuerreira(){
-            
-            if(vida > 0){
-                vida -= 2;
-                System.out.println(nome+" tenta desviar do golpe de guitarra de "+oponente+" e infelizmente não consegue :(  ");
-                System.out.println("+5 de dano :( Vida atual : "+vida);
-            }
-            else{
-                System.out.println("Fim de jogo para"+nome+" :( Parabebéns "+oponente);
-            }
-
-
-        }
-        public void receberDanoGuerreiraEspecial(){
-            if(vida > 0 ){
-                vida -= 10;
-                System.out.println(nome+" tenta desviar do golpe de guitarra de "+oponente+" e infelizmente não consegue :(  ");
-                System.out.println("+10 de dano :( Vida atual : "+vida);
-            }
-            else{
-                System.out.println("Fim de jogo para"+nome+" :( Parabebéns "+oponente);
-            }
-
-
-        }
-        public void usarGuitarra(){
-            forca += 4; 
-            mana -= 4;
-            System.out.println(nome+" lança sua mágia negra para atacar"+oponente);
-            System.out.println("+2 de força!! Força atual: "+forca);
-        }
-        public void subirDeNivel(){
-            if(mana == 75 && forca == 50){
-                System.out.println("   Parabéns você subiu de nível!!");
-                mana += 10;
-                forca += 2;
-                vida += 10;
-                nivel += 1;
-            }
-        }
-        public void mostrarStatus(){
-            System.out.println
-            ("Nome: "+nome+" | Classe: Guerreira | Vida: "+vida+" | Força: "+forca+" | Mana: "+mana);
-        }
-
-
-
-
-
-    }
-
-    }
+}
